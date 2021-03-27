2d9e75f30f003d6784cb459747603c2b842336c4
Mamunur Rashid <rimonmath@gmail.com>
2021-03-27 21:08:20 +0600

    Part 34 done

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,68 @@
 <template>
+  <header>
+    <h2>{{ msg }}</h2>
+  </header>
   <div>
-    <h1>{{ msg }}</h1>
-    <contact-details
-      v-for="contact in contacts"
-      :key="contact.name"
-      :name="contact.name"
-      :website-address="contact.website"
-      :address="contact.address"
-    ></contact-details>
+    <the-card cardTitle="About Me">
+      <p>Hi, I am Mamunur Rashid Rimon, blah blah blah ...</p>
+    </the-card>
+    <the-card cardTitle="Apple iPhone 12 Pro">
+      <img
+        src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro--.jpg"
+        alt=""
+      />
+      <p>
+        Versions: A2407 (International); A2341 (USA); A2406 (Canada, Japan);
+        A2408 (China, Hong Kong)
+      </p>
+    </the-card>
+    <the-card cardTitle="Services">
+      <ul>
+        <li>Web Development</li>
+        <li>Software Development</li>
+        <li>Mobile app Development</li>
+      </ul>
+    </the-card>
+
+    <the-card cardTitle="Test card"> </the-card>
   </div>
 </template>
 
 <script>
-import ContactDetails from "./ContactDetails.vue";
+import TheCard from "./TheCard.vue";
 
 export default {
   data() {
     return {
-      msg: "Welcome to Vue3 Bangla Tutorial",
-
-      contacts: [
-        {
-          name: "Mamunur Rashid",
-          website: "http://rimonbd.com",
-          address: "Dhaka, Bangladesh"
-        },
-        {
-          name: "Rimon",
-          website: "http://rimoned.com",
-          address: "Rajshahi, Bangladesh"
-        },
-        {
-          name: "Rashid",
-          website: "http://radhis.com",
-          address: "Pabna, Bangladesh"
-        }
-      ]
+      msg: "Vue3 Bangla Tutorial"
     };
   },
   components: {
-    ContactDetails
+    TheCard
   }
 };
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+  line-height: 1.8;
+}
+
+header {
+  background-color: #1f2c46;
+  color: #41b883;
+  font-size: 22px;
+  padding: 22px;
+}
+
+header h2 {
+  text-align: center;
+}
+</style>
---
+++ src/TheCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="the-card">
+    <div class="the-card__title">
+      {{ cardTitle }}
+    </div>
+    <div class="the-card__body">
+      <slot>
+        Default card content goes here
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cardTitle"]
+};
+</script>
+
+<style>
+.the-card {
+  width: 200px;
+  border: 1px solid #006f88;
+  min-height: 200px;
+  margin: 22px auto;
+}
+
+.the-card__title {
+  background: #167db1;
+  padding: 5px 11px;
+  color: #fff;
+}
+
+.the-card__body {
+  padding: 11px;
+}
+
+.the-card__body img {
+  height: 111px;
+}
+</style>
